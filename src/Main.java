f187ea85a2a59adc09f4a8e63651785fb8a2c87f
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-06 22:45:42 +0300

    add two methods

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //   constructor =  special method that is called when an object is instantiated  (created)
+
+        Human human1 = new Human("Edwin", 29, 60);
+        Human human2 = new Human("James", 43, 65);
+
+
+
+        human2.eat();
+        human2.drink();
+
+
+        human1.eat();
+        human1.drink();
+
+
+
     }
 }
